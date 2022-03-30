6b1899f34130e0687e528b47fb128304060b227e
shubham kumar <shubhamkumar252083@gmail.com>
2022-03-30 12:43:11 +0530

    Coinbase Charges implementation is working

--- payments/urls.py
+++ payments/urls.py
@@ -5,4 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home_view, name='payments-home'),
+    path('success/', views.success_view, name='payments-success'),  # new
+    path('cancel/', views.cancel_view, name='payments-cancel'),  # new
 ]
--- payments/views.py
+++ payments/views.py
@@ -1,5 +1,33 @@
+from coinbase_commerce.client import Client
 from django.shortcuts import render
 
-# Create your views here.
+from core import settings
+
+
 def home_view(request):
-    return render(request, 'home.html', {})
+    client = Client(api_key=settings.COINBASE_COMMERCE_API_KEY)
+    domain_url = 'http://localhost:8000/'
+    product = {
+        'name': 'ebradsProduct',
+        'description': 'A really good product',
+        'local_price': {
+            'amount': '1.00',
+            'currency': 'USD'
+        },
+        'pricing_type': 'fixed_price',
+        'redirect_url': domain_url + 'success/',
+        'cancel_url': domain_url + 'cancel/',
+    }
+    charge = client.charge.create(**product)
+
+    return render(request, 'home.html', {
+        'charge': charge,
+    })
+
+
+def success_view(request):
+    return render(request, 'success.html', {})
+
+
+def cancel_view(request):
+    return render(request, 'cancel.html', {})
